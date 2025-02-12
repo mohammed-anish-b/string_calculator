bd754340e848b4dbc231812d1ee116abf7a650dd
Mohammed Anish B <mohammedanish000@gmail.com>
2025-02-12 08:45:36 +0530

    Return number itself for single input

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -5,4 +5,8 @@ void main() {
   test('Empty string should return 0', () {
     expect(add(""), equals(0));
   });
+
+  test('Single number should return itself', () {
+    expect(add("1"), equals(1));
+  });
 }
